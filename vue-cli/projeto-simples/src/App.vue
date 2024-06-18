2ca3a3c3321ad2fafb4b9b309c3f496f1127be3a
jonata <jonatanery2016@gmail.com>
2024-06-18 10:03:16 -0400

    finalizando o modulo inicial de componentes

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,24 @@
+<!--<template>
+  <div id="app">
+    {{ titulo }}
+    <button @click="titulo += '#'">alterar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      titulo: 'Teste data usando Vue JS!!!'
+    }
+  }
+}
+</script>
+<style>
+  #app{
+    background-color: chocolate;
+    color: #FFFFFF;
+  }
+</style>-->
+
+<!--a palavra scaffold monta toda essa estrutura pra voce com template,script,style-->
